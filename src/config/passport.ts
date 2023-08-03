944b01fb592e1fd932809ed3a4ad3a6b4a0ee8d0
Felipe Galobart Jora <felipe@mitsuwa.com.br>
2023-08-03 16:00:50 -0300

    Fazendo JWT com passport 1

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,5 +1,20 @@
 import passport from "passport";
+import dotenv from 'dotenv';
+import { Strategy as JWTStrategy, ExtractJwt } from "passport-jwt";
 
+dotenv.config();
+
+const notAuthorizedJson = { status: 401, message: 'Não Autorizado' };
+
+const options = {
+  jwtFromRequest: 
+}
+
+passport.use(new JWTStrategy(options, (payload, done) => {
+
+  return done(null, user)
+  return done(notAuthorizedJson, false)
+}));
 
 
 export default passport;
